6cabf9aa67cbb85ffa3f372dc9400c917c309f90
Alexander Zaznobin <alex.zaznobin@gmail.com>
2023-05-07 21:48:24 +0300

    driver mistakes 10

--- mysql_scraper.py
+++ mysql_scraper.py
@@ -222,6 +222,7 @@ def add_id_from_sql (dataframe, df_column, db_column, db_table_name, engine) :
         with engine.connect() as conn :
             query = conn.execute(text(sql_query))
         sql_df = pd.DataFrame(query.fetchall())
+        print('add_id_from_sql query.fetchall()',sql_df)
         try:
             result=dataframe.merge(sql_df, how='inner', left_on=df_column, right_on=db_column)
         except:
