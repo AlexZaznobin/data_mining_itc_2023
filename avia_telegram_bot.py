21bda0399b7e791d4fb9412175f4787116c293e8
Alexander Zaznobin <alex.zaznobin@gmail.com>
2023-05-12 01:58:46 +0300

    add telegram bot interface 2

--- avia_telegram_bot.py
+++ avia_telegram_bot.py
@@ -17,6 +17,7 @@ import datetime
 import logging
 import pandas as pd
 import os
+
 TOKEN = "6199462379:AAGZ9Qmzs1uIejPrYnknj0hn3eIL12OVDm0"
 
 logging.basicConfig(level=logging.INFO)
@@ -29,14 +30,14 @@ dp = Dispatcher(bot, storage=storage)
 
 
 # States
-class Form(StatesGroup):
+class Form(StatesGroup) :
     start_point = State()  # Will be represented in storage as 'Form:name'
     end_point = State()  # Will be represented in storage as 'Form:age'
     date_range = State()  # Will be represented in storage as 'Form:gender'
 
 
 @dp.message_handler(commands='start')
-async def cmd_start(message: types.Message):
+async def cmd_start (message: types.Message) :
     """
     Conversation's entry point
     """
@@ -45,13 +46,12 @@ async def cmd_start(message: types.Message):
     await message.reply("Hi there! In what city would we start journey?")
 
 
-
 @dp.message_handler(state=Form.start_point)
-async def process_start_point(message: types.Message, state: FSMContext):
+async def process_start_point (message: types.Message, state: FSMContext) :
     """
     Process start_point
     """
-    async with state.proxy() as data:
+    async with state.proxy() as data :
         data['start_point'] = message.text
 
     await Form.next()
@@ -59,11 +59,11 @@ async def process_start_point(message: types.Message, state: FSMContext):
 
 
 @dp.message_handler(state=Form.end_point)
-async def process_end_point(message: types.Message, state: FSMContext):
+async def process_end_point (message: types.Message, state: FSMContext) :
     """
     Process start_point
     """
-    async with state.proxy() as data:
+    async with state.proxy() as data :
         data['end_point'] = message.text
 
     await Form.next()
@@ -71,10 +71,9 @@ async def process_end_point(message: types.Message, state: FSMContext):
                         "(e.g if you want to check tickets in July enter 01073107)")
 
 
-
 @dp.message_handler(state=Form.date_range)
-async def process_date_range(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
+async def process_date_range (message: types.Message, state: FSMContext) :
+    async with state.proxy() as data :
         data['date_range'] = message.text
         # markup = types.ReplyKeyboardRemove()
         print("start_point",
@@ -85,16 +84,16 @@ async def process_date_range(message: types.Message, state: FSMContext):
 
         pd.set_option('display.max_columns', None)
         config = load_scraper_config()
-        need_database=False
+        need_database = True
         intiniate_result_file(config['result_file'])
         intiniate_result_file(config['last_request_data'])
         start = datetime.datetime.now()
         if config['use_proxy'] == 1 :
             save_file_api_proxy_list(config)
 
-        start_list=list(get_relevant_airport_codes(data['start_point']).values)
-        end_list=list(get_relevant_airport_codes(data['end_point']).values)
-        url_list= get_url_list(start_list=start_list,
+        start_list = list(get_relevant_airport_codes(data['start_point']).values)
+        end_list = list(get_relevant_airport_codes(data['end_point']).values)
+        url_list = get_url_list(start_list=start_list,
                                 start_date=get_date_range(data['date_range'])[0],
                                 days_number=get_date_range(data['date_range'])[1],
                                 end_list=end_list,
@@ -110,44 +109,51 @@ async def process_date_range(message: types.Message, state: FSMContext):
         if need_database :
             make_api_price_request(config, logging)
         if os.path.exists(config['last_request_data']) :
-            last_request_df=pd.read_csv(config['last_request_data'])
-            analytics_file='avia-analytics.xls'
+            last_request_df = pd.read_csv(config['last_request_data'])
+            last_request_df = last_request_df[last_request_df['price'] != 'price']
+            last_request_df = last_request_df[last_request_df['price'] != 'None']
+            last_request_df = last_request_df[last_request_df['price'] != 'NaN']
+            last_request_df = last_request_df[~last_request_df['price'].isna()]
+            analytics_file = 'avia-analytics.xls'
             save_df_to_excel(last_request_df, analytics_file)
 
-            with open(analytics_file, 'rb') as file:
+            with open(analytics_file, 'rb') as file :
                 await bot.send_document(message.chat.id, types.InputFile(file), caption=analytics_file)
 
             os.remove(analytics_file)
 
     await bot.send_message(
-            message.chat.id,
-            md.text(
-                "Lets find some tickets, my friend",
-                md.text('start_point,', md.code(data['start_point'])),
-                md.text('end_point:', md.code(data['end_point'])),
-                md.text('data_range:', md.code(data['date_range'])),
-                url_list[:10],
-                sep='\n',
-            ),
-            # reply_markup=markup,
-            # parse_mode=ParseMode.MARKDOWN,
-        )
+        message.chat.id,
+        md.text(
+            "Lets find some tickets, my friend",
+            md.text('start_point,', md.code(data['start_point'])),
+            md.text('end_point:', md.code(data['end_point'])),
+            md.text('data_range:', md.code(data['date_range'])),
+            url_list[:10],
+            sep='\n',
+        ),
+        # reply_markup=markup,
+        # parse_mode=ParseMode.MARKDOWN,
+    )
     #
     # # Finish conversation
     await state.finish()
 
-def get_relevant_airport_codes(city_name):
+
+def get_relevant_airport_codes (city_name) :
     # Normalize the city name by converting it to lowercase and removing leading/trailing spaces
     config = load_scraper_config()
     city_name = city_name.lower().strip()
-    airport_codes=choose_closet_airports(config, city_name)
+    airport_codes = choose_closet_airports(config, city_name)
     print(type(airport_codes))
     return airport_codes
 
-def save_df_to_excel(df, filename):
+
+def save_df_to_excel (df, filename) :
     writer = pd.ExcelWriter(filename, engine='openpyxl')
     df.to_excel(writer, index=False)
     writer.save()
 
-if __name__ == '__main__':
+
+if __name__ == '__main__' :
     executor.start_polling(dp, skip_updates=True)
